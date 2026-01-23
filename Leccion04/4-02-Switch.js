2a3181830919d246084d4c6315ba13257bbfbc27
carlosninacio <cigramirez@poligran.edu.co>
2026-01-22 21:43:01 -0500

    ejercicio con varios case agrupados con una misma respuesta

--- Leccion04/4-02-Switch.js
+++ Leccion04/4-02-Switch.js
@@ -16,3 +16,25 @@ switch(numero) {
 }
 
 console.log(numeroTexto);
+
+let mes = 19;
+let estacion = "Estacion desconocidad";
+
+switch (mes) {
+    case 1: case 2: case 12:
+        estacion = "Invierno";
+        break;
+    case 3: case 4: case 5:
+        estacion = "Primmavera";
+        break;
+    case 6: case 7: case 8:
+        estacion = "Verano";
+        break;
+    case 9: case 10: case 11:
+        estacion = "Otoño";
+        break;
+    default:
+        estacion = "Valor incorrecto";
+}
+
+console.log(estacion);
