ce22b35654736eb5724997c691678cc72bda5fec
carlosninacio <cigramirez@poligran.edu.co>
2026-01-25 11:05:31 -0500

    Paso por referencia

--- Leccion07/07-01-Funciones.js
+++ Leccion07/07-01-Funciones.js
@@ -47,3 +47,19 @@ function sumarTodo() {
     }
     return suma;
 }
+
+// Paso por referencia
+const persona = {
+    nombre: 'Juan',
+    apellido: 'Perez'
+}
+
+console.log(persona);
+
+function cambiarValorObjeto(p1) {
+    p1.nombre = 'Carlos';
+    p1.apellido = 'Ramirez';
+}
+
+cambiarValorObjeto(persona);
+console.log(persona);
