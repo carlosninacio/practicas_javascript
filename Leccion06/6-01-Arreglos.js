8b7130a3c72ad891cde210225a9d7758fb122133
carlosninacio <cigramirez@poligran.edu.co>
2026-01-25 09:53:21 -0500

    Declaración de arreglos y recorrer arreglos con for

---
+++ Leccion06/6-01-Arreglos.js
@@ -0,0 +1,8 @@
+// let autos = new Array('BMW', 'Mercedes Benz', 'Volvo'); // Forma antigua de declarar arreglo
+const autos = ['BMW', 'Mercedes Benz', 'Volvo'];
+console.log(autos);
+console.log(autos[0]);
+
+for(let i = 0; i < autos.length; i++) {
+    console.log(i + ': ' + autos[i]);
+}
