639efb216b3fbf08d25cd120e9c79d4fb1cd29c0
carlosninacio <cigramirez@poligran.edu.co>
2026-01-25 10:13:27 -0500

    Mas formas de declarar funciones

--- Leccion07/07-01-Funciones.js
+++ Leccion07/07-01-Funciones.js
@@ -1,7 +1,21 @@
-miFuncion(4,6); // Se puede llamar antes de definirla
+miFuncion(4,6); // Se puede llamar antes de definirla (Hoisting)
 
 function miFuncion(a, b) {
     console.log("Suma: " + (a+b));
 }
 
 miFuncion(2,3);
+
+
+function miFuncion1(a,b) {
+    return a + b;
+}
+
+let resultado = miFuncion1(2,3);
+console.log(resultado);
+
+// Declaración función de tipo expresion
+let x = function (a,b) {return a + b};
+
+resultado = x(1,2);
+console.log(resultado);
