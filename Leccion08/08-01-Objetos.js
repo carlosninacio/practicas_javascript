14bdb36ca20265687f3eaa36ac17e4ff3423bfdb
carlosninacio <cigramirez@poligran.edu.co>
2026-01-25 11:38:58 -0500

    Modificar propiedades de objetos

--- Leccion08/08-01-Objetos.js
+++ Leccion08/08-01-Objetos.js
@@ -1,5 +1,3 @@
-let x = 10;
-
 let persona = {
     nombre: 'Karen',
     apellido: 'Perez',
@@ -15,4 +13,29 @@ console.log(persona.apellido);
 console.log(persona.edad);
 console.log(persona.nombreCompleto());
 
+console.log(persona);
+
+
+// Forma 2 de crear objetos, menos común
+let persona2 = new Object();
+persona2.nombre = 'Carlos';
+persona2.direccion = 'Avenida 8E Quinta Oriental';
+persona2.cel = '3004669565';
+
+console.log(persona2.cel)
+console.log(persona2);
+
+// Mas formas de acceder a propiedades del objeto
+console.log(persona['email']);
+
+for(nombrePropiedad in persona) {
+    console.log(nombrePropiedad);
+    console.log(persona[nombrePropiedad]);
+}
+
+persona.tel = '55443322'; // Se crea nueva propiedad
+persona.tel = '44332211'; // Se modifica la propiedad
+console.log(persona);
+
+delete persona.tel; // Eliminar propiedad
 console.log(persona);
