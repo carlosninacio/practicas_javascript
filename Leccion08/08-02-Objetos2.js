b674321e99391c72060bd3a100aa7edd1cd6695d
carlosninacio <cigramirez@poligran.edu.co>
2026-01-26 15:08:33 -0500

    Set y constructor

--- Leccion08/08-02-Objetos2.js
+++ Leccion08/08-02-Objetos2.js
@@ -1,11 +1,28 @@
+// Constructor de objetos de tipo Persona
+function Persona(nombre, apellido, email) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.email = email;
+}
+
 let persona = {
     nombre: 'Karen',
     apellido: 'Perez',
     email: 'karen@gmail.com',
     edad: 18,
+    idioma: 'es',
+    get lang() {
+        return this.idioma.toUpperCase();
+    },
+    set lang(lang) {
+        this.idioma = lang.toUpperCase();
+    },
     get nombreCompleto() {
         return this.nombre + ' ' + this.apellido;
     }
 }
 
 console.log(persona.nombreCompleto); // Con get no es necesario los parentesis
+console.log(persona.lang);
+persona.lang = 'es_col';
+console.log(persona.lang);
