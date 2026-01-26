dcf7a8780afc0977c7d13ca31cc48e3013b96862
carlosninacio <cigramirez@poligran.edu.co>
2026-01-26 15:47:18 -0500

    Metodo call y aply para objetos

--- Leccion08/08-03-Objetos.js
+++ Leccion08/08-03-Objetos.js
@@ -1,8 +1,8 @@
 let persona1 = {
     nombre: 'Juan',
     apellido: 'Perez',
-    nombreCompleto: function() {
-        return this.nombre + ' ' + this.apellido;
+    nombreCompleto: function(titulo, tel) {
+        return titulo + ': ' +this.nombre + ' ' + this.apellido + ' ' + tel;
     }
 }
 
@@ -12,6 +12,12 @@ let persona2 = {
 }
 
 // Uso de call para usar el metodo persona1.nombreCompleto con los datos de persona2
-console.log(persona1.nombreCompleto());
+console.log(persona1.nombreCompleto('Lic', '3183314474'));
+console.log(persona1.nombreCompleto.call(persona2, 'Ing', '3004669565')); // Metodo call
 
-console.log(persona1.nombreCompleto.call(persona2)); // Metodo call
+// Uso de apply
+console.log(persona1.nombreCompleto('Prof', '321323132'));
+
+let arreglo = ['Ing', '55443322']
+console.log(persona1.nombreCompleto.apply(persona2, arreglo));
+console.log(persona1.nombreCompleto.apply(persona2, ['Arq', '663399']));
