a69f99527cc9e11f9c1ffa892eec3b5edc3e5def
carlosninacio <cigramirez@poligran.edu.co>
2026-01-26 15:22:36 -0500

    Formas simplificadas de crear objetos

--- Leccion08/08-02-Objetos2.js
+++ Leccion08/08-02-Objetos2.js
@@ -3,6 +3,10 @@ function Persona(nombre, apellido, email) {
     this.nombre = nombre;
     this.apellido = apellido;
     this.email = email;
+    this.nombreCompleto = function() {
+        return this.nombre + ' ' + this.apellido;
+    }
+    
 }
 
 let persona = {
@@ -26,3 +30,33 @@ console.log(persona.nombreCompleto); // Con get no es necesario los parentesis
 console.log(persona.lang);
 persona.lang = 'es_col';
 console.log(persona.lang);
+
+let padre = new Persona('Juan', 'Perez', 'juan@gmail.com');
+console.log(padre);
+
+
+let madre = new Persona('Juana', 'Perez', 'juanitaperez@gmail.com');
+console.log(madre);
+
+padre.nombre = 'Carlos';
+console.log(padre);
+console.log(padre.nombreCompleto());
+
+let miObjeto = new Object();
+let miObjeto2 = {};
+
+let miCadena1 = new String('Hola');
+let miCadena2 = 'Hola';
+
+let miNumero = new Number(1);
+let miNumero2 = 1;
+
+let miBoolean = new Boolean(true);
+let miBoolean2 = true;
+
+let miArreglo1 = new Array();
+let miArreglo2 = [];
+
+let miFuncion = new Function();
+let miFuncion2 = function() {
+};
