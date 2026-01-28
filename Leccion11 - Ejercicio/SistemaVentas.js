ce827e45520c5c13f29a96215899d8502bdc66c8
carlosninacio <cigramirez@poligran.edu.co>
2026-01-28 18:24:43 -0500

    Ejercicio SistemaVentas finalizado

--- Leccion11 - Ejercicio/SistemaVentas.js
+++ Leccion11 - Ejercicio/SistemaVentas.js
@@ -2,7 +2,7 @@ class Producto {
     static contadorProductos = 0;
 
     constructor(nombre, precio) {
-        this._idProducto = ++contadorProductos;
+        this._idProducto = ++Producto.contadorProductos;
         this._nombre = nombre;
         this._precio = precio;
     }
@@ -28,6 +28,67 @@ class Producto {
     }
     
     toString() {
-        return `idProducto: ${this._idProducto}, nombre: ${this._nombre}, precio: ${this._precio}`;
+        return `idProducto: ${this._idProducto}, nombre: ${this._nombre}, precio: $${this._precio}`;
     }
 }
+
+class Orden {
+    static contadorOrdenes = 0;
+
+    static get MAX_PRODUCTOS() {
+        return 5;
+    }
+
+    constructor() {
+        this._idOrden = ++Orden.contadorOrdenes;
+        this._productos = [];
+        //this._contadorProductosAgregados = 0;
+    }
+
+    get idOrden() {
+        return this._idOrden;
+    }
+
+    agregarProducto(producto) {
+        if (this._productos.length < Orden.MAX_PRODUCTOS) {
+            this._productos.push(producto);
+            //this._productos[this._contadorProductosAgregados++] = producto;
+        } else {
+            console.log('No se pueden agregar más productos');
+        }
+    }
+
+    calcularTotal() {
+        let totalVenta = 0;
+        for (let producto of this._productos) {
+            totalVenta += producto.precio;
+        }
+        return totalVenta;
+    }
+
+    mostraOrden() {
+        let productosOrden = '';
+        for (let producto of this._productos) {
+            productosOrden += ' {' +  producto.toString() + '}';
+        }
+        console.log(`Orden: ${this._idOrden} Total: ${this.calcularTotal()}, Productos: ${productosOrden}`)
+    }
+}
+
+let producto1 = new Producto('Pantalon', 200);
+let producto2 = new Producto('Camisa', 100);
+// console.log(producto1.toString());
+// console.log(producto2.toString());
+
+let orden1 = new Orden();
+orden1.agregarProducto(producto1);
+orden1.agregarProducto(producto2);
+// orden1.mostraOrden();
+
+let orden2 = new Orden();
+let producto3 = new Producto('Cinturon', 50);
+orden2.agregarProducto(producto1);
+orden2.agregarProducto(producto2);
+orden2.agregarProducto(producto2);
+orden2.agregarProducto(producto3);
+orden2.mostraOrden();
