5f3686724b523d5006953b62ce73791d08b5a58b
carlosninacio <cigramirez@poligran.edu.co>
2026-02-03 17:30:17 -0500

    Try, Catch y Finally

---
+++ Leccion15/15-01-TryCatch.js
@@ -0,0 +1,12 @@
+'use strict'
+
+try {
+    let x=10;
+    //miFuncion();
+} catch(error) {
+    console.log(error);
+} finally { // Opcional
+    console.log('continuar');
+}
+
+console.log('fuera del try-catch')
