b43c87206eee660aa5c98b2c13a3a83a93c49853
carlosninacio <cigramirez@poligran.edu.co>
2026-02-03 18:00:27 -0500

    Mas funciones flecha

--- Leccion16/16-01-FuncionesFlecha.js
+++ Leccion16/16-01-FuncionesFlecha.js
@@ -19,3 +19,34 @@ let miFuncionFlecha = () => {
 }
 
 miFuncionFlecha();
+
+const miFuncionFlecha1 = () => console.log('Saludos desde funcion flecha sin abrir cuerpo');
+
+miFuncionFlecha1();
+
+const saludar = () => {
+    return 'Saludos desde función flecha';
+}
+
+console.log(saludar());
+
+const saludar1 = () => 'Saludos desde función flecha';
+console.log(saludar1());
+
+const regresarObjeto = () => ({nombre: 'Carlos', apellido: 'Ramirez'});
+console.log(regresarObjeto());
+
+// const funcionConParametros = (mensaje) => console.log(mensaje);
+const funcionConParametros = mensaje => console.log(mensaje);
+funcionConParametros('Saludos con parametros');
+
+const funcionConParametrosClasico = function (mensaje) {
+    console.log(mensaje);
+}
+
+// const funcionConVariosParametros = (op1,op2) => op1 + op2;
+const funcionConVariosParametros = (op1,op2) => {
+    let resultado = op1 + op2;
+    return `Resultado: ${resultado}`;
+}
+console.log(funcionConVariosParametros(2,3));
