55f46d1a8eded3ebaaf47e04ad04f7143c0d0f47
carlosninacio <cigramirez@poligran.edu.co>
2026-02-03 17:22:44 -0500

    Poliformismo

--- Leccion14/14-01-POO.js
+++ Leccion14/14-01-POO.js
@@ -15,10 +15,28 @@ class Gerente extends Empleado {
         this.departamento = departamento;
     }
 
+    // Sobreescritura de metodo
     obtenerDetalles() {
         return `Gerente -> ${super.obtenerDetalles()} depto: ${this.departamento}`
     }
 }
 
+function imprimir(tipo) {
+    console.log(tipo.obtenerDetalles());
+    if (tipo instanceof Gerente) {
+        console.log('Es un objeto de tipo gerente');
+    } if (tipo instanceof Empleado) {
+        console.log('Es un tipo empleado');
+    } if (tipo instanceof Object) {
+        console.log('Es un tipo object');
+    }
+}
+
+let empleado1 = new Empleado('Karen', 20000);
+console.log(empleado1.obtenerDetalles());
+
 let gerente1 = new Gerente('Carlos', 15000, 'Sistemas');
 console.log(gerente1.obtenerDetalles());
+
+imprimir(gerente1);
+imprimir(empleado1);
