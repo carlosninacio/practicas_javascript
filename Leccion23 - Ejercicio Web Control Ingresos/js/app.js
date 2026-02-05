dcff4423cb260a763a45914d16659197e72a3c8b
carlosninacio <cigramirez@poligran.edu.co>
2026-02-05 16:04:53 -0500

    Metodos para calcular ingresos y egresos, y mostrarlos en el index

--- Leccion23 - Ejercicio Web Control Ingresos/js/app.js
+++ Leccion23 - Ejercicio Web Control Ingresos/js/app.js
@@ -7,3 +7,32 @@ const egresos = [
     new Egreso('Comida', 500),
     new Egreso('Gym', 250)
 ];
+
+let cargarApp = () => {
+    cargarCabecero();
+}
+
+let totalIngresos = () => {
+    let totalIngreso = 0;
+    for (let ingreso of ingresos) {
+        totalIngreso += ingreso.valor;
+    }
+    return totalIngreso;
+}
+
+let totalEgresos = () => {
+    let totalEgreso = 0;
+    for(let egreso of egresos) {
+        totalEgreso += egreso.valor;
+    }
+    return totalEgreso;
+}
+
+let cargarCabecero = () => {
+    let presupuesto = totalIngresos() - totalEgresos();
+    let porcentajeEgreso = totalEgresos()/totalIngresos();
+    document.getElementById('presupuesto').innerHTML = presupuesto;
+    document.getElementById('porcentaje').innerHTML = porcentajeEgreso;
+    document.getElementById('ingresos').innerHTML = totalIngresos();
+    document.getElementById('egresos').innerHTML = totalEgresos();
+}
